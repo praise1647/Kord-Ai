2169dc0881f23fbfe3fd40f07b6f5b5d3f5c2117
praise1647 <praisedavid2222@gmail.com>
2025-03-31 15:16:22 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -4,7 +4,7 @@ global.settings = {
   OWNER_NUMBERS: "2348142200423", // your number with country code
   OWNER_NAME: 'Praise', // your owner name 
   BOT_NAME: "KORD", // your preferred bot name
-  SESSION_ID: 'kord_ai-OaulhPgwMdN4', // replace your_sessionid with yours and make sure its inside rhe quotes
+  SESSION_ID: 'kord_ai-1qG73edNp9zk', // replace your_sessionid with yours and make sure its inside rhe quotes
   INVINCIBLE_MODE: false, // toogle invincible mode 
   WORK_MODE: "Public",
   TIME_ZONE: "Africa/Lagos",
